af240984c4c326bc080773b813e2363920ed983f
Santiago Agustin Laiton Cubides <santiago.laiton@mail@escuelaing.edu.co>
2020-09-23 20:56:13 -0500

    lab 06

---
+++ src/main/java/edu/eci/cvds/servlet/App.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.servlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
